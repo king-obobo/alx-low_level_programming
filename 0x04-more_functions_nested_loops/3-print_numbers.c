9b3d20172877776798ff67d78310196f30eb1a65
king-obobo <oboboebuka1@gmail.com>
2022-10-20 16:04:28 -0700

    No 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints 0 - 9
+  * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= 9; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
