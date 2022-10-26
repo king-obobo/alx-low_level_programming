23ef4b7306f54f14917a67cfa10b50244d3323fe
king-obobo <oboboebuka1@gmail.com>
2022-10-26 09:57:11 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+/**
+ * rev_string - reverses a string
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	char temp;
+	int i, len, len1;
+
+	len = len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < lem / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len1];
+		s[len--] = tmp;
+	}
+}
