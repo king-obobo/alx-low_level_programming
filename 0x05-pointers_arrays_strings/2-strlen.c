59b6b031653f7e6fc698ab6f3e57c0d34227ee42
king-obobo <oboboebuka1@gmail.com>
2022-10-26 09:22:24 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string parameter input
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
