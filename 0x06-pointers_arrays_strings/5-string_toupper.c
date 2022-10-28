6a7b927008e70e9ff925e48ea0e783431115ef8a
king-obobo <oboboebuka1@gmail.com>
2022-10-28 06:43:31 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+ * string_toupper - a function that changes all
+ *             lowercase letters of a string to uppercase.
+ * @s : pointer to input char
+ *
+ * Return: @s
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
