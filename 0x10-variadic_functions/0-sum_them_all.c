26f57aa60cb0d5e9da2ce3170e662af5988cb86c
king-obobo <oboboebuka1@gmail.com>
2022-11-18 11:44:21 -0800

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - adds all its parameters
+ * @n: start of input variables
+ * Return: the sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is repo containing tasks for 0x10. C - Variadic functions
