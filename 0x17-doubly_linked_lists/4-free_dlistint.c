333177742aba3694a17792206e99754bcc17cfee
king-obobo <oboboebuka1@gmail.com>
2023-01-13 13:47:14 -0800

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: head of the list
+ * Return: no return
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+
+	while (head != NULL)
+	{
+		dlistint_t *next = head->next;
+
+		free(head);
+		head = next;
+	}
+}
