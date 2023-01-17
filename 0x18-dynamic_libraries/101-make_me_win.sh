699ebb2c31e19ccfa1b2551f9dc746fb99fcf336
king-obobo <oboboebuka1@gmail.com>
2023-01-16 16:49:08 -0800

    Task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Mickykore/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinject.so
-export LD_PRELOAD=/tmp/libinject.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
