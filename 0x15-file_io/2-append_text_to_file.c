1c1957ed5821d8f59042a673fa06beec15e7df88
king-obobo <oboboebuka1@gmail.com>
2023-06-05 06:26:08 -0700

    Task 2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+* append_text_to_file - appends text at the end of a file
+ * @filename: file name to operate on
+ * @text_content: NULL terminated text to append to file
+ * Return: 1 (success), -1 (fail)
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd, w;
+
+if (filename != NULL)
+{
+fd = open(filename, O_WRONLY | O_APPEND);
+if (text_content != NULL)
+w = write(fd, text_content, _strlen(text_content));
+
+if (fd == -1 || w == -1)
+return (-1);
+
+close(fd);
+return (1);
+}
+return (-1);
+}
+
+/**
+* _strlen - gets string length
+ * @str: NULL terminated character string
+ * Return: int
+*/
+int _strlen(char *str)
+{
+int len = 0;
+
+while (str[len] != '\0'
+len++;
+
+return (len);
+}
+
