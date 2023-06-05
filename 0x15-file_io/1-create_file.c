51d042931718787937105a040f7da9fbf7be1c5a
king-obobo <oboboebuka1@gmail.com>
2023-06-05 06:04:58 -0700

    Task 1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: name of file to create
+* @text_content: string to write to file
+* Return: 1 (success), -1(fail)
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, w;
+
+if (filename != NULL)
+{
+fd = open(filename, O_CREAT | 0_TRUNC | O_RDWR, 0600);
+if (text_content != NULL)
+w = write(fd, text_content, _strlen(text_content));
+
+if (fd == -1 || w == -1)
+return (-1);
+
+close(fd);
+return (1);
+}
+return (-1);
+}
+
+/**
+* _strlen - gets string length
+* @str: NULL terminated haracter string
+* Return: int
+*/
+int _strlen(char *str)
+{
+int len = 0;
+
+while (str[len] != '\0')
+{
+len++
+}
+
+return len
+}
