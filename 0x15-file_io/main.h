8478e9c47deaec1b412a7845f5a10acbf9ce9145
king-obobo <oboboebuka1@gmail.com>
2023-06-05 06:09:56 -0700

    Task 1. Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,17 +4,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #include <unistd.h>
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+int _strlen (char *str);
+int cp (char *file_from, char *file_to);
 
 #endif
